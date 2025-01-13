b0f3eba69db7386fdaa1fe64629d4e8a9cea5e64
fatsfa <fathifathima808@gmail.com>
2025-01-13 15:40:07 +0400

    first

---
+++ RECIPE/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Search from './Components/Search'
+import FoodList from './Components/FoodList'
+import Nav from './Components/Nav'
+import "./App.css"
+
+function App() {
+  const [foodData, setFoodData] = useState([])
+
+
+  return (
+    <>
+  <div className = "App">
+  <Nav/>
+  <Search foodData= {foodData} setFoodData = {setFoodData} />
+  <FoodList foodData= {foodData}/>
+  </div>
+  </>
+  )
+}
+
+export default App
